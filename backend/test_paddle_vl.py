e9282e96f7191efbd9ec3179213091a7c933aaef
sanatK24~ <sanat.karkhanis2@gmail.com>
2026-06-02 21:30:54 +0530

    refactor DB and OCRcleanup

--- backend/test_paddle_vl.py
+++
@@ -1,7 +0,0 @@
-try:
-    from paddleocr import PaddleOCRVL
-    pipeline = PaddleOCRVL(pipeline_version="v1.5")
-    print("SUCCESS")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
